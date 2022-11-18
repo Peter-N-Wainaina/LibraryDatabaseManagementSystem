33be0592c19ba7890f4c1bc5c3868aaedf80acfd
Peter-N-Wainaina <pnw6@cornell.edu>
2022-11-18 18:07:50 -0500

    Formatted the way commands are printed

--- src/command.ml
+++ src/command.ml
@@ -11,37 +11,47 @@ type command =
   | Help
   | Logout
 
-let student_options () =
-  [
-    "Borrowed Books";
-    "Favorite Books";
-    "Fiction";
-    "Autobiography";
-    "Biography";
-    "Fantasy";
-    " HistoricalFiction";
-    "Memoir";
-    "Mystery";
-    "NonFiction";
-    "Novel";
-    "Philosophy";
-    "Religion";
-    "ScienceFiction";
-    "Thriller";
-  ]
+let book_categories =
+  "To access book categories: \n\t\t Borrowed Books\n \t\t Favorite Books\n"
+
+let genres =
+  "To access books be genre: \n\
+  \ \t\tFiction \n\
+  \  \t\tAutobiography\n\
+  \ \t\tBiography \n\
+   \t\tPhilosophy;\n\
+  \     \t\tFantasy\n\
+  \ \t\tHistoricalFiction \n\
+  \ \t\tMemoir\n\
+  \ \t\tMystery \n\
+  \ \t\tNonFiction\n\
+  \ \t\tNovel\n\
+  \ \t\tReligion\n\
+  \ \t\tScienceFiction \n\
+  \ \t\tThriller\n\n\
+  \   "
+
+let student_options () = [ book_categories; genres ]
 
 let student_help () =
   [
-    "\n\
-    \         Borrowed books - see the name of the books you have borrowed from \n\
-    \        Cornell Libraries";
-    "Favorite books - see the name of the books you have marked as favorite \n\
-    \        before.";
-    "You can access books by any of the following genre by typing :  \
-     Autobiography,  Biography,  Fantasy , Fiction , HistoricalFiction , Memoir\n\
-    \    , Mystery , NonFiction , Novel , Philosophy , Religion , \
-     ScienceFiction ,\n\
-    \    Thriller. ";
+    "Borrowed books: Lists the names of all the books you have borrowed";
+    "Favorite books: Lists the names of the books you have marked as favorite ";
+    "You can access books by any of the following genre by typing :\n\
+    \  \t\tAutobiography\n\
+     \t\tBiography\n\
+     \t\tFantasy \n\
+     \t\tFiction\n\
+    \  \t\tHistoricalFiction\n\
+    \  \t\tMemoir\n\
+    \     \t\tMystery\n\
+    \  \t\tNonFiction\n\
+    \  \t\tNovel\n\
+    \  \t\tPhilosophy\n\
+    \  \t\tReligion \n\
+     \t\tScienceFiction\n\
+     \t\tThriller\n\
+    \     ";
   ]
 
 let librarian_options () = []
